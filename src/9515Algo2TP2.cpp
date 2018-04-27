81f5f3bf794604494b32ae19ca5113a8cdb6531b
Joel Perez <joperezr@gmail.com>
2018-04-27 00:47:14 -0300

    Agrego primeros archivos al repositorio para probar GIT desde Linux.

---
+++ src/9515Algo2TP2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 9515Algo2TP2.cpp
+// Author      : Grupo Neo
+// Version     :
+// Copyright   : Copyright  2018
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
