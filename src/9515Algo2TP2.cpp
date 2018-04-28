8f48fed0cf63f94f709b004b58d02542a240c737
veronica <veronica@veronica-VirtualBox>
2018-04-28 17:01:02 -0300

    agrego mi linea en el libreto... jaja

--- src/9515Algo2TP2.cpp
+++ src/9515Algo2TP2.cpp
@@ -13,5 +13,6 @@ int main() {
 	cout << "T.P. Nro. 2 - Granjeros." << endl;
 	cout << "Hola chicos, como estan, soy Analia?" << endl;
 	cout << "Hola! Soy Joel haciendo cambios sobre el commit de Ana..." << endl;
+	cout << "soy vero!!" << endl;
 	return 0;
 }
