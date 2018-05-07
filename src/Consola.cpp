7d5363833d1a59f50fcf8329c4d84254ed6a75f1
gabriela <gabriela@gabriela-X455LA>
2018-05-07 20:48:55 -0300

    nuevo

--- src/Consola.cpp
+++ src/Consola.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void MostrarBienvenida() {
-	cout << "T.P. Nro. 2 - Granjeros." << endl;
+	cout << "=) T.P. Nro. 2 - Granjeros." << endl;
 	cout << "  		   ..:::Bienvenidos:::.." << endl << endl;
 	cout << "El juego consiste en maximizar las ganancias del jugador" << endl;
 }
