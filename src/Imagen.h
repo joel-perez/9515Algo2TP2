ca8938080a3eedfd2fc059468eb26743e9492ffd
veronica <veronicaleguizamon@outlook.es>
2018-05-12 20:37:04 -0300

    Agrego TDA Imagen para el EasyBMP comentado, no hace absolutamente nada

---
+++ src/Imagen.h
@@ -0,0 +1,50 @@
+
+#ifndef IMAGEN_H_
+#define IMAGEN_H_
+
+/*
+ * Necesita > Dimensiones, capacidad del Tanque y cantidad, capacidad del almmacen y cantidad, nombre del jugador,
+ */
+
+/*
+#include "lib/EasyBMP.h"
+#include "lib/EasyBMP_Geometry.h"
+#include "lib/EasyBMP_Font.h"
+
+//class Imagen{
+//private:
+//	RGBApixel color;
+	BMP cultivo;
+	BMP fondoTerreno;
+	BMP fondoUsuario;
+	BMP imagenDelTerreno;
+*/
+//public:
+	/*
+	 * PRE
+	 * POST dimensiones de la imagen final creada.
+	 */
+	//Imagen();
+
+	/*
+	 * PRE
+	 * POST imagen del estado del terreno en cada turno creado.
+	 */
+	//void obtenerImagenDelTerreno(char* jugador);
+
+//private:
+	/*
+	 * PRE
+	 * POST adecua las medidas del cultivo.
+	 */
+	//void rescalarImagenes();
+	/*
+	 * PRE
+	 * POST coloca los fondos del disenio en la imagen final.
+	 */
+	//void pegarFondos();
+//};
+
+
+#endif
+
