a81c01d1489580e68a8401e2bd7983dbfa323a01
gabriela <nadehsko@hotmail.com>
2018-05-19 11:03:45 -0300

    algo xd

--- src/Cosecha.cpp
+++ src/Cosecha.cpp
@@ -1,7 +1,11 @@
 #include "Cosecha.h"
 
 Cosecha::Cosecha() {
+	this->cosechas= new Lista<Cultivo*>;
 
 }
 
+Lista<Cultivo*>* Cosecha::obtenerCosechas(){
+	return this->cosechas;
+}
 //TODO: Implementar los metodos necesarios...
--- src/Cosecha.h
+++ src/Cosecha.h
@@ -1,15 +1,26 @@
 #ifndef COSECHA_H_
 #define COSECHA_H_
+#include "Lista.h"
+#include "Cultivo.h"
 
 class Cosecha {
 private:
+	Lista<Cultivo*>* cosechas;
+
 
 public:
 	/*
 	 * PRE:
-	 * POST:
+	 * POST:cosecha lista para usar.
 	 */
 	Cosecha();
+	/*
+	 * PRE:
+	 * POST:Devuelve los las cosechas del jugadorActual.
+	 */
+	Lista<Cultivo*>* obtenerCosechas();
 };
+
+
 
 #endif
