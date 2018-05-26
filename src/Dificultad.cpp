7bc29a5355571fc1ac8ead602acf1985eec60c14
Joel Perez <joperezr@gmail.com>
2018-05-26 20:41:19 -0300

    Configuro bien las dificultades...

--- src/Dificultad.cpp
+++ src/Dificultad.cpp
@@ -36,39 +36,39 @@ Dificultad::Dificultad(TipoDificultad dificultadSeleccionada) {
 }
 
 void Dificultad::inicializarDificultadFacil() {
-	this->coeficienteTamanioTanque = 1;
-	this->coeficienteCreditosAsignados = 1;
+	this->coeficienteTamanioTanque = 3;
+	this->coeficienteCreditosAsignados = 3;
 	this->coeficientePrecioCultivo = 1;
 	this->coeficienteCantidadTurnos = 1;
-	this->coeficienteMultiplicadorAgua = 1;
+	this->coeficienteMultiplicadorAgua = 3;
 	this->coeficientePrecioTerreno = 1;
-	this->coeficienteTamanioAlmacen = 1;
+	this->coeficienteTamanioAlmacen = 3;
 	this->coeficienteTamanioTerreno = 1;
-	this->coeficienteTamanioTanque = 1;
+	this->coeficienteTamanioTanque = 3;
 }
 
 void Dificultad::inicializarDificultadIntermedio() {
 	this->coeficienteTamanioTanque = 2;
 	this->coeficienteCreditosAsignados = 2;
 	this->coeficientePrecioCultivo = 2;
-	this->coeficienteCantidadTurnos = 2;
+	this->coeficienteCantidadTurnos = 1;
 	this->coeficienteMultiplicadorAgua = 2;
 	this->coeficientePrecioTerreno = 2;
 	this->coeficienteTamanioAlmacen = 2;
-	this->coeficienteTamanioTerreno = 2;
+	this->coeficienteTamanioTerreno = 1;
 	this->coeficienteTamanioTanque = 2;
 }
 
 void Dificultad::inicializarDificultadDificil() {
-	this->coeficienteTamanioTanque = 3;
-	this->coeficienteCreditosAsignados = 3;
+	this->coeficienteTamanioTanque = 1;
+	this->coeficienteCreditosAsignados = 1;
 	this->coeficientePrecioCultivo = 3;
-	this->coeficienteCantidadTurnos = 3;
-	this->coeficienteMultiplicadorAgua = 3;
+	this->coeficienteCantidadTurnos = 1;
+	this->coeficienteMultiplicadorAgua = 1;
 	this->coeficientePrecioTerreno = 3;
-	this->coeficienteTamanioAlmacen = 3;
-	this->coeficienteTamanioTerreno = 3;
-	this->coeficienteTamanioTanque = 3;
+	this->coeficienteTamanioAlmacen = 1;
+	this->coeficienteTamanioTerreno = 1;
+	this->coeficienteTamanioTanque = 1;
 }
 
 unsigned int Dificultad::obtenerCoeficienteCreditosAsignados() {
