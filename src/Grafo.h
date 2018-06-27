8ef4e862f4a07103dbdb59e4856765865feace3d
Joel Perez <joperezr@gmail.com>
2018-06-27 19:31:21 -0300

    Completo un poco el template de Grafo.

--- src/Grafo.h
+++ src/Grafo.h
@@ -1,6 +1,8 @@
 #ifndef GRAFO_H_
 #define GRAFO_H_
 
+#include "Nodo.h"
+
 /*
  * Es un TDA contenedor de un conjunto de datos llamados nodos y de un conjunto
  * de aristas cada una de las cuales se determina mediante un par de nodos.
@@ -20,37 +22,37 @@ public:
 	 * PRE: que el grafo exista  y que el nodo no esté previamente.
 	 * POST: el grafo queda modificado por el agregado del nuevo nodo.
 	 */
-	void InsertarNodo();
+	void InsertarNodo(std::string nombre);
 
 	/* Inserta una arista nueva, recibida como argumento, en el grafo.
 	 * PRE: que el grafo exista , que la arista no esté previamente y que existan en el grafo los nodos origen y destino de la arista.
 	 * POST: el grafo queda modificado por el agregado de la nueva arista.
 	 */
-	void InsertarArista();
+	void InsertarArista(Nodo<T>* origen, Nodo<T>* destino, int peso);
 
 	/* Elimina un nodo, recibido como argumento,  del grafo.
 	 * PRE: que el grafo exista  y que el nodo a eliminar esté en él y no tenga aristas incidentes en él.
 	 * POST: el grafo queda modificado por la eliminación del nodo.
 	 */
-	void EliminarNodo();
+	void EliminarNodo(Nodo<T>* nodo);
 
 	/* Elimina una arista, recibida como argumento,  del grafo.
 	 * PRE: que el grafo exista  y la arista estén él.
 	 * POST: el grafo queda modificado por la eliminación de la arista.
 	 */
-	void EliminarArista();
+	void EliminarArista(Nodo<T>* origen, Nodo<T>* destino);
 
 	/* Recibe una arista y retorna un valor logico indicando si la arista existe en el grafo.
 	 * PRE: que el grafo exista.
 	 * POST: --- 
 	 */
-	bool ExisteArista();
+	bool ExisteArista(Nodo<T>* origen, Nodo<T>* destino);
 
 	/* Recibe una arista y retorna un valor logico indicando si el nodo existe en el grafo.
 	 * PRE: que el grafo exista.
 	 * POST: ---
 	 */
-	bool ExisteNodo();
+	bool ExisteNodo(std::string nombre);
 
 	/* Destruye el grafo.
 	 * PRE: que el grafo exista.
@@ -59,5 +61,42 @@ public:
 	~Grafo();
 
 };
+
+template<class T> Grafo<T>::Grafo() {
+	//TODO: Implementar...
+}
+
+template<class T> void Grafo<T>::InsertarNodo(std::string nombre) {
+	//TODO: Implementar...
+}
+
+template<class T> void Grafo<T>::InsertarArista(Nodo<T>* origen,
+		Nodo<T>* destino, int peso) {
+	//TODO: Implementar...
+}
+
+template<class T> void Grafo<T>::EliminarNodo(Nodo<T>* nodo) {
+	//TODO: Implementar...
+}
+
+template<class T> void Grafo<T>::EliminarArista(Nodo<T>* origen,
+		Nodo<T>* destino) {
+	//TODO: Implementar...
+}
+
+template<class T> bool Grafo<T>::ExisteArista(Nodo<T>* origen,
+		Nodo<T>* destino) {
+	//TODO: Implementar...
+	return false;
+}
+
+template<class T> bool Grafo<T>::ExisteNodo(std::string nombre) {
+	//TODO: Implementar...
+	return false;
+}
+
+template<class T> Grafo<T>::~Grafo() {
+	//TODO: Implementar...
+}
 
 #endif
